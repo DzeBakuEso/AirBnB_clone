8ad8077b50cd601df2d89c87133afcdf320b7966
DzeBakuEso <dzeblebaku@gmail.com>
2024-11-17 13:55:12 -0800

    models/state.py, models/city.py, models/amenity.py, models/place.py, models/review.py, tests/

---
+++ models/city.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+from models.base_model import BaseModel
+
+class City(BaseModel):
+    """City class inherits from BaseModel"""
+    state_id = ""  # it will be the State.id
+    name = ""
